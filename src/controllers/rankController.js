37750cfa01bee08b883b6ec5ec63b7f2adf8663b
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-06-08 18:14:14 +0900

    Feat : [Rank] 요청값 검증 middleware 제작

--- src/controllers/rankController.js
+++ src/controllers/rankController.js
@@ -1,13 +1,8 @@
 import { rankService } from '../services/rankService.js';
-import { UnauthorizedError } from '../middlewares/errorMiddleware.js';
 
 class rankController {
     static async rankList(req, res, next) {
         const userId = req.currentUserId;
-
-        if (!userId) {
-            throw new UnauthorizedError('로그인한 유저만 사용할 수 있는 서비스입니다.');
-        }
 
         try {
             const getRank = await rankService.getRankList({ userId });
