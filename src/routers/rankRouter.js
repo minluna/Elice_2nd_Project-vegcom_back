754d4d3e327286ec47fa0c95213c50e612162803
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-06-17 06:42:26 +0000

    Revert "Merge branch 'dev' into 'master'"
    This reverts merge request !2

--- src/routers/rankRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { rankController } from '../controllers/rankController.js';
-
-const rankRouter = Router();
-
-rankRouter.get('/list', rankController.rankList);
-
-export { rankRouter };
